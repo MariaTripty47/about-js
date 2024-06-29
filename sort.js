3690da92515b108ce7c87929f0c94a7277c28caf
tishalaa <marianurtripty47@gmail.com>
2024-06-29 16:01:08 +0600

    addeded a new javascript file named sort.js

---
+++ sort.js
@@ -0,0 +1,32 @@
+// const persons = ['Elon','Bob','Maria','Sara','Rashid'];
+
+// const sortedPerson = persons.sort();
+
+// console.log(sortedPerson);
+
+// -----ascending-descending------
+// for single numbers
+// const numbers =[9,5,7,8,4,2];
+
+//  const numbers_asc = numbers.sort()
+
+//  console.log(numbers_asc);
+
+// // ---ascending for double numbers
+
+// const numbers =[4,7,12,8,43,6,1];
+
+//  const numbers_asc = numbers.sort(function(a,b) {return a-b})
+
+//  console.log(numbers_asc);
+
+ 
+// ---descending for double numbers
+
+const numbers =[4,7,12,8,43,6,1];
+
+ const numbers_asc = numbers.sort(function(a,b) {return b-a})
+
+ console.log(numbers_asc);
+
+
