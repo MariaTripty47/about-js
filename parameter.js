b19fce35f86aef603c6f82bbead1254891474e93
tishalaa <marianurtripty47@gmail.com>
2024-06-30 22:09:53 +0600

    //

---
+++ parameter.js
@@ -0,0 +1,33 @@
+function eatFood(){
+    console.log('do hand wash');
+    console.log('take the plate');
+    console.log('sit and eat the food');
+}
+
+eatFood();
+
+
+
+function square(number){
+
+const square = number*number;
+
+console.log('value of the number parameter', number);
+
+console.log('square of the given number is:', square);
+
+}
+
+square(5);
+
+
+function add(num1, num2){
+
+    const sum = num1 + num2;
+
+    console.log(sum);
+
+    }
+
+add(7,88);
+add(80,10)
