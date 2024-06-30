5342d14f5e156d2ce237255d4b1f4d2aac5730a0
tishalaa <marianurtripty47@gmail.com>
2024-06-30 20:48:13 +0600

    addeded a new javascript file

---
+++ function.js
@@ -0,0 +1,27 @@
+// function declaration
+
+function closeThefan(){
+
+    console.log('stand up');
+    console.log('walk towards the switch');
+    console.log('click the switch to off the fan');
+
+}
+// call the function
+
+   closeThefan();
+
+// function name(){
+// 
+// }
+
+function brushTeeth(){
+
+    console.log('pick up the brush');
+    console.log('add paste');
+    console.log('clean your teeth properly');
+
+
+}
+
+brushTeeth();
